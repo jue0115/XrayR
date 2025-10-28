07a3cba7dfc7803314623a7f675d9412620cc945
jue115 <46835138+jue0115@users.noreply.github.com>
2025-10-28 19:40:15 +0800

    update

--- panel/panel.go
+++ panel/panel.go
@@ -154,7 +154,7 @@ func (p *Panel) loadCore(panelConfig *Config) *core.Instance {
 	if err != nil {
 		log.Panicf("failed to create instance: %s", err)
 	}
-	log.Print("Xray Core Version: %s", core.Version())
+	log.Printf("Xray Core Version: %s", core.Version())
 
 	return server
 }
